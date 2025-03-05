57bf253264f8929f7b851b2dcecc773486ea09f7
MeesCode <meesgamez@gmail.com>
2025-03-05 15:40:47 +0100

    cleanup

--- main/main.c
+++ main/main.c
@@ -10,8 +10,8 @@
 #include "temp_sensor.h"
 
 static const char *TAG = "MAIN";
-bool booted = false;
-bool light_state = false;
+bool zb_connected = false;
+bool switch_state = false;
 
 const float target_temp = 30.0;
 const float temp_offset = 0.5;
@@ -54,16 +54,16 @@ static void temp_task(void *pvParameters)
     for(;;){
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        if(!booted) continue;
+        if(!zb_connected) continue;
 
         float temp = get_temp();
         report_temperature(temp);
 
-        // roughly keep temp at 25c
-        if(temp < target_temp - temp_offset){
+        // roughly keep temp at 25c by turning on/off the heater
+        if(temp < target_temp - temp_offset && switch_state){
             gpio_set_level(GPIO_NUM_1, 1);
             report_output_binary_sensor(1);
-        } else if(temp > target_temp + temp_offset){
+        } else if(temp > target_temp + temp_offset || !switch_state){
             gpio_set_level(GPIO_NUM_1, 0);
             report_output_binary_sensor(0);
         }
@@ -87,12 +87,13 @@ static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t 
     if (message->info.dst_endpoint == HA_ESP_ENDPOINT){
         if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF){
             if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL){
-                light_state = message->attribute.data.value ? *(bool *)message->attribute.data.value : light_state;
-                gpio_set_level(GPIO_NUM_15, !light_state);
-                ESP_LOGI(TAG, "Light sets to %s", light_state ? "On" : "Off");
+                switch_state = message->attribute.data.value ? *(bool *)message->attribute.data.value : switch_state;
+                gpio_set_level(GPIO_NUM_15, !switch_state);
+                ESP_LOGI(TAG, "Light sets to %s", switch_state ? "On" : "Off");
             }
         }
     }
+    
     return ret;
 }
 
@@ -129,8 +130,8 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
                 ESP_LOGI(TAG, "Start network steering");
                 esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
             } else {
-                ESP_LOGI(TAG, "Device rebooted");
-                booted = true;
+                ESP_LOGI(TAG, "Device connected");
+                zb_connected = true;
             }
         } else {
             ESP_LOGW(TAG, "%s failed with status: %s, retrying", esp_zb_zdo_signal_to_string(sig_type), esp_err_to_name(err_status));
@@ -146,7 +147,7 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
                      extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
                      extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
                      esp_zb_get_pan_id(), esp_zb_get_current_channel(), esp_zb_get_short_address());
-            booted = true;
+            zb_connected = true;
         } else {
             ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
             esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
@@ -244,7 +245,7 @@ static void esp_zb_task(void *pvParameters)
         .u.send_info.max_interval = 0,
         .u.send_info.def_min_interval = 1,
         .u.send_info.def_max_interval = 0,
-        .u.send_info.delta.u16 = 5,
+        .u.send_info.delta.u16 = 10,
         .u.send_info.reported_value.u16 = 0,
         .attr_id = ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID,
         .manuf_code = ESP_ZB_ZCL_ATTR_NON_MANUFACTURER_SPECIFIC,
