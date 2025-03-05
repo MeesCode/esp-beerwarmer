835602c26a6bf16970eb4f8de084b4349a204d3f
MeesCode <meesgamez@gmail.com>
2025-03-05 14:31:58 +0100

    cleanup and add binary output for heater

--- main/main.c
+++ main/main.c
@@ -13,6 +13,9 @@ static const char *TAG = "MAIN";
 bool booted = false;
 bool light_state = false;
 
+const float target_temp = 30.0;
+const float temp_offset = 0.5;
+
 #define DEFINE_PSTRING(var, str)   \
     const struct                   \
     {                              \
@@ -20,35 +23,50 @@ bool light_state = false;
         char content[sizeof(str)]; \
     }(var) = {sizeof(str) - 1, (str)}
 
+
+void report_output_binary_sensor(uint16_t value)
+{
+    esp_zb_zcl_set_attribute_val(
+        HA_ESP_ENDPOINT, //
+        ESP_ZB_ZCL_CLUSTER_ID_BINARY_INPUT, 
+        ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, 
+        ESP_ZB_ZCL_ATTR_BINARY_INPUT_PRESENT_VALUE_ID, 
+        &value, 
+        false
+    );
+}
+
+void report_temperature(float temp)
+{
+    uint16_t temp_zb = (uint16_t)(temp*100.);
+    esp_zb_zcl_set_attribute_val(
+        HA_ESP_ENDPOINT, //
+        ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT, 
+        ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, 
+        ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, 
+        &temp_zb, 
+        false
+    );
+}
+
 static void temp_task(void *pvParameters)
 {
-    while (1)
-    {
-        if(booted){
-
-            float temp = get_temp();
-
-            // roughly keep temp at 25c
-            if(temp < 25){
-                gpio_set_level(GPIO_NUM_1, 1);
-            } else{
-                gpio_set_level(GPIO_NUM_1, 0);
-            }
-
-            uint16_t temp_zb = (uint16_t)(temp*100.);
-            esp_zb_zcl_set_attribute_val(
-                HA_ESP_LIGHT_ENDPOINT, //
-                ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT, 
-                ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, 
-                ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID, 
-                &temp_zb, 
-                false
-            );
-        } else{
-            ESP_LOGI(TAG, "Device not booted yet");
-        }
-        
+    for(;;){
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        if(!booted) continue;
+
+        float temp = get_temp();
+        report_temperature(temp);
+
+        // roughly keep temp at 25c
+        if(temp < target_temp - temp_offset){
+            gpio_set_level(GPIO_NUM_1, 1);
+            report_output_binary_sensor(1);
+        } else if(temp > target_temp + temp_offset){
+            gpio_set_level(GPIO_NUM_1, 0);
+            report_output_binary_sensor(0);
+        }
     }
 }
 
@@ -63,14 +81,12 @@ static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t 
     ESP_RETURN_ON_FALSE(message, ESP_FAIL, TAG, "Empty message");
     ESP_RETURN_ON_FALSE(message->info.status == ESP_ZB_ZCL_STATUS_SUCCESS, ESP_ERR_INVALID_ARG, TAG, "Received message: error status(%d)",
                         message->info.status);
-    ESP_LOGI(TAG, "Received message: endpoint(0x%x), cluster(0x%x), attribute(0x%x), data size(%d)", message->info.dst_endpoint, message->info.cluster,
-             message->attribute.id, message->attribute.data.size);
-    if (message->info.dst_endpoint == HA_ESP_LIGHT_ENDPOINT)
-    {
-        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF)
-        {
-            if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL)
-            {
+    ESP_LOGI(TAG, "Received message: endpoint(0x%x), cluster(0x%x), attribute(0x%x), data size(%d)", message->info.dst_endpoint, message->info.cluster, message->attribute.id, message->attribute.data.size);
+
+    // handle light on/off
+    if (message->info.dst_endpoint == HA_ESP_ENDPOINT){
+        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF){
+            if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL){
                 light_state = message->attribute.data.value ? *(bool *)message->attribute.data.value : light_state;
                 gpio_set_level(GPIO_NUM_15, !light_state);
                 ESP_LOGI(TAG, "Light sets to %s", light_state ? "On" : "Off");
@@ -97,7 +113,7 @@ static esp_err_t zb_action_handler(esp_zb_core_action_callback_id_t callback_id,
 
 void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
 {
-    uint32_t *p_sg_p       = signal_struct->p_app_signal;
+    uint32_t *p_sg_p = signal_struct->p_app_signal;
     esp_err_t err_status = signal_struct->esp_err_status;
     esp_zb_app_signal_type_t sig_type = *p_sg_p;
     switch (sig_type) {
@@ -117,8 +133,7 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
                 booted = true;
             }
         } else {
-            ESP_LOGW(TAG, "%s failed with status: %s, retrying", esp_zb_zdo_signal_to_string(sig_type),
-                     esp_err_to_name(err_status));
+            ESP_LOGW(TAG, "%s failed with status: %s, retrying", esp_zb_zdo_signal_to_string(sig_type), esp_err_to_name(err_status));
             esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb,
                                    ESP_ZB_BDB_MODE_INITIALIZATION, 1000);
         }
@@ -131,15 +146,14 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
                      extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
                      extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
                      esp_zb_get_pan_id(), esp_zb_get_current_channel(), esp_zb_get_short_address());
-            booted = 1;
+            booted = true;
         } else {
             ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
             esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
         }
         break;
     default:
-        ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
-                 esp_err_to_name(err_status));
+        ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type, esp_err_to_name(err_status));
         break;
     }
 }
@@ -190,17 +204,26 @@ static void esp_zb_task(void *pvParameters)
     };
     esp_zb_attribute_list_t *esp_zb_temperature_meas_cluster = esp_zb_temperature_meas_cluster_create(&temperature_meas_cfg);
 
+    // cluster binary sensor
+    esp_zb_binary_input_cluster_cfg_t binary_input_cfg = {
+        .out_of_service = false,
+        .status_flags = 0,
+        .present_value = 0,
+    };
+    esp_zb_attribute_list_t *esp_zb_binary_input_cluster = esp_zb_binary_input_cluster_create(&binary_input_cfg);
+
     // create cluster list
     esp_zb_cluster_list_t *esp_zb_cluster_list = esp_zb_zcl_cluster_list_create();
     esp_zb_cluster_list_add_basic_cluster(esp_zb_cluster_list, esp_zb_basic_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
     esp_zb_cluster_list_add_identify_cluster(esp_zb_cluster_list, esp_zb_identify_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
     esp_zb_cluster_list_add_on_off_cluster(esp_zb_cluster_list, esp_zb_on_off_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
     esp_zb_cluster_list_add_temperature_meas_cluster(esp_zb_cluster_list, esp_zb_temperature_meas_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    esp_zb_cluster_list_add_binary_input_cluster(esp_zb_cluster_list, esp_zb_binary_input_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
 
     // create endpoint list
     esp_zb_ep_list_t *esp_zb_ep_list = esp_zb_ep_list_create();
     esp_zb_endpoint_config_t endpoint_config = {
-        .endpoint = HA_ESP_LIGHT_ENDPOINT,
+        .endpoint = HA_ESP_ENDPOINT,
         .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID,
         .app_device_id = ESP_ZB_HA_ON_OFF_LIGHT_DEVICE_ID,
     };
@@ -210,22 +233,31 @@ static void esp_zb_task(void *pvParameters)
     esp_zb_device_register(esp_zb_ep_list);
     esp_zb_core_action_handler_register(zb_action_handler);
 
-    // setup automatic reporting
+    // setup automatic reporting for temperature
     esp_zb_zcl_reporting_info_t reporting_info = {
         .direction = ESP_ZB_ZCL_CMD_DIRECTION_TO_SRV,
-        .ep = 10,
+        .ep = HA_ESP_ENDPOINT,
         .cluster_id = ESP_ZB_ZCL_CLUSTER_ID_TEMP_MEASUREMENT,
         .cluster_role = ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
         .dst.profile_id = ESP_ZB_AF_HA_PROFILE_ID,
-        .u.send_info.min_interval = 1,
         .u.send_info.max_interval = 0,
-        .u.send_info.def_min_interval = 1,
-        .u.send_info.def_max_interval = 0,
-        .u.send_info.delta.u16 = 10,
         .attr_id = ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_ID,
         .manuf_code = ESP_ZB_ZCL_ATTR_NON_MANUFACTURER_SPECIFIC,
     };
     esp_zb_zcl_update_reporting_info(&reporting_info);
+
+    // setup automatic reporting for binary input
+    esp_zb_zcl_reporting_info_t reporting_info_binary = {
+        .direction = ESP_ZB_ZCL_CMD_DIRECTION_TO_SRV,
+        .ep = HA_ESP_ENDPOINT,
+        .cluster_id = ESP_ZB_ZCL_CLUSTER_ID_BINARY_INPUT,
+        .cluster_role = ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
+        .dst.profile_id = ESP_ZB_AF_HA_PROFILE_ID,
+        .u.send_info.min_interval = 1,
+        .attr_id = ESP_ZB_ZCL_ATTR_BINARY_INPUT_PRESENT_VALUE_ID,
+        .manuf_code = ESP_ZB_ZCL_ATTR_NON_MANUFACTURER_SPECIFIC,
+    };
+    esp_zb_zcl_update_reporting_info(&reporting_info_binary);
 
     // start zigbee
     esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
@@ -237,7 +269,7 @@ void app_main(void)
 {
     // setup onboard led
     gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_15, 0);
+    gpio_set_level(GPIO_NUM_15, 1);
 
     // setup heater gpio
     gpio_set_direction(GPIO_NUM_1, GPIO_MODE_OUTPUT);
